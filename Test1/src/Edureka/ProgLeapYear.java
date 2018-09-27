90d60a22a07e6fddfba9a786a81cd4124e652fe5
Shuchi <Shuchi@192.168.0.104>
2018-09-28 01:20:27 +0530

    leapyear

--- Test1/src/Edureka/ProgLeapYear.java
+++ Test1/src/Edureka/ProgLeapYear.java
@@ -3,48 +3,22 @@ package Edureka;
 public class ProgLeapYear {
 
 	public static void main(String[] args) {
-		 int a=1900, b=2014, c= 2004, d=2000;
+		 int[] arr = new int[4];
+		 arr[0]=1900; arr[1]=2014; arr[2]= 2004; arr[3]=2000;
 		 
-		 if(a%4==0 && a%400!=0) {
-			 System.out.println(a+ " is a non century leap year");
-		 }
-		 else
-			 {if(a%400==0 && a%4==0)
-			 System.out.println(a+ " is a century leap year");
-		 else 
-			 System.out.println(a+ " is not a leap year");
+		 for(int i=0;i<arr.length;i++)
+		 { 
+			 if( arr[i]%400==0) {
+				 System.out.println(arr[i]+ " is a century leap year");
 			 }
+			 else if(arr[i]%4==0) 
+				 System.out.println(arr[i]+ " is a non century leap year");
+				 
+			 else 
+				 System.out.println(arr[i]+ " is not a leap year");
+				  
+		 }
 		 
-		 if(b%4==0 && b%400!=0) {
-			 System.out.println(b+ " is a non century leap year");
-		 }
-		 else
-			 {if(b%400==0 && b%4==0)
-			 System.out.println(b+ " is a century leap year");
-		 else 
-			 System.out.println(b+ " is not a leap year");
-			 }
-		
-		 if(c%4==0 && c%400!=0) {
-			 System.out.println(c+ " is a non century leap year");
-		 }
-		 else
-			 {if(c%400==0 && c%4==0)
-			 System.out.println(c+ " is a century leap year");
-		 else 
-			 System.out.println(c+ " is not a leap year");
-			 }
-		 
-		 if(d%4==0 && d%400!=0) {
-			 System.out.println(d+ " is a non century leap year");
-		 }
-		 else
-			 {if(d%400==0 && d%4==0)
-			 System.out.println(d+ " is a century leap year");
-		 else 
-			 System.out.println(d+ " is not a leap year");
-			 }
-		
 	}
 
 }
